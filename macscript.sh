e2fc4e4c5e4cb36a8f05ff0353714a69af788faa
Marko <90ts@mail.ru>
2023-04-28 00:31:09 +0300

    macOS

--- macscript.sh
+++ macscript.sh
@@ -1,155 +1,127 @@
 #!/bin/bash
 
-i=0
-a=0
-b=0
-c=0
-d=0
+framescounter=0
+units=0
+dozens=0
+hundreds=0
+thousands=0
 
 fn_bye() { echo "Bye bye."; exit 0; }
 
 fn_fail() { echo "Wrong option."; exit 1; }
 
 dir() {
-    echo -ne "
-Print the folder: "
-    read -r dir
-    if [[ $dir == "Exit" ]]
-    then
-        fn_bye
-    elif [[ $dir == "Back" ]]
-    then
-        clear
-        mainmenu
-    elif [[ ! -n $dir ]]
-    then
-        dir
-    elif [[ ! -d $dir ]]
-    then     
-        mkdir $dir
-        if [[ zxc -eq 0 ]]
-        then
-            fn_url
-        else
-            fn_path
-        fi
+  echo -ne "Print the folder: "
+  read -r dir
+  if [[ $dir == "Exit" ]] || [[ $dir == "exit" ]] || [[ $dir == "EXIT" ]]; then
+    fn_bye
+  elif [[ $dir == "Back" ]] || [[ $dir == "back" ]] || [[ $dir == "BACK" ]]; then
+    clear
+    mainmenu
+  elif [[ ! -n $dir ]]; then
+    dir
+  elif [[ ! -d $dir ]]; then     
+    mkdir $dir
+    if [[ counter -eq 0 ]]; then
+      fn_url
     else
-        if [[ zxc -eq 0 ]]
-        then
-            fn_url
-        else
-            fn_path
-        fi
+      fn_path
     fi
+  else
+    if [[ counter -eq 0 ]]; then
+      fn_url
+    else
+      fn_path
+    fi
+  fi
 }
 
 fn_url() { 
-    TMPDIR=$(mktemp -d)
-    curl -sL $anssubmenu -o $TMPDIR/0000.$strurl
-    ffmpeg -i $TMPDIR/0000.$strurl $dir/%04d.jpg
-    for file in $(ls $dir)
-    do
-        i=$(( $i + 1 ))
-        a=$(( $i % 10 ))
-        b=$(( $i % 100 / 10 ))
-        c=$(( $i % 1000 / 100 ))
-        d=$(( $i % 10000 / 1000 ))
-        open $dir/$d$c$b$a.jpg
-        sleep 0.5
-        clear
-        osascript -e 'quit app "Preview"'
-    done
+  TMPDIR=$(mktemp -d)
+  curl -sL $anssubmenu -o $TMPDIR/0000.$strurl
+  ffmpeg -i $TMPDIR/0000.$strurl $dir/%04d.jpg
+  for file in $(ls $dir); do
+    framescounter=$(( $framescounter + 1 ))
+    units=$(( $framescounter % 10 ))
+    dozens=$(( $framescounter % 100 / 10 ))
+    hundreds=$(( $framescounter % 1000 / 100 ))
+    thousands=$(( $framescounter % 10000 / 1000 ))
+    open $dir/$d$c$b$a.jpg
+    sleep 0.5
+    clear
+    osascript -e 'quit app "Preview"'
+  done
 }
 
 fn_path() {
-    ffmpeg -i $anspath $dir/%04d.jpg
-    for file in $(ls $dir)
-    do
-        i=$(( $i + 1 ))
-        a=$(( $i % 10 ))
-        b=$(( $i % 100 / 10 ))
-        c=$(( $i % 1000 / 100 ))
-        d=$(( $i % 10000 / 1000 ))
-        open $dir/$d$c$b$a.jpg
-        sleep 0.5
-        clear
-        osascript -e 'quit app "Preview"'
-    done
+  ffmpeg -i $anspath $dir/%04d.jpg
+  for file in $(ls $dir); do
+    framescounter=$(( $framescounter + 1 ))
+    units=$(( $framescounter % 10 ))
+    dozens=$(( $framescounter % 100 / 10 ))
+    hundreds=$(( $framescounter % 1000 / 100 ))
+    thousands=$(( $framescounter % 10000 / 1000 ))
+    open $dir/$d$c$b$a.jpg
+    sleep 0.5
+    clear
+    osascript -e 'quit app "Preview"'
+  done
 }
 
 urlsubmenu() {
-    echo -ne "
-Print the URL: "
-    read -r anssubmenu
-    strurl=$(echo $anssubmenu | rev | awk -F '.' '{print $1}' | rev)
-    arrurl=(mp4 gif webm mkv)
-    if [[ ${arrurl[@]} =~ $strurl ]]
-    then
-        zxc=0
-        dir
-    elif [[ $anssubmenu == "Exit" ]]
-    then
-        fn_bye
-    elif [[ $anssubmenu == "Back" ]]
-    then
-        clear
-        mainmenu
-    elif [[ ! -n $anssubmenu ]]
-    then
-        urlsubmenu
-    else
-        fn_fail
-    fi
+  echo -ne "Print the URL: "
+  read -r anssubmenu
+  strurl=$(echo $anssubmenu | rev | awk -F '.' '{print $1}' | rev)
+  arrurl=(mp4 gif webm mkv)
+  if [[ ${arrurl[@]} =~ $strurl ]]; then
+    counter=0
+    dir
+  elif [[ $anssubmenu == "Exit" ]] || [[ $anssubmenu == "exit" ]] || [[ $anssubmenu == "EXIT" ]]; then
+    fn_bye
+  elif [[ $anssubmenu == "Back" ]] || [[ $anssubmenu == "back" ]] || [[ $anssubmenu == "BACK" ]]; then
+    clear
+    mainmenu
+  elif [[ ! -n $anssubmenu ]]; then
+    urlsubmenu
+  else
+    fn_fail
+  fi
 }
 
 pathsubmenu() {
-    echo -ne "
-Print the PATH: "
-    read -r anspath
-    strpath=$(echo $anspath | rev | awk -F '.' '{print $1}' | rev)
-    arrpath=(mp4 gif webm mkv)
-    if [[ ${arrpath[@]} =~ $strpath ]]
-    then
-        zxc=1
-        dir
-    elif [[ $anspath == "Exit" ]]
-    then
-        fn_bye
-    elif [[ $anspath == "Back" ]]
-    then
-        clear
-        mainmenu
-    elif [[ ! -n $anspath ]]
-    then
-        pathsubmenu
-    else
-        fn_fail
-    fi
+  echo -ne "Print the PATH: "
+  read -r anspath
+  strpath=$(echo $anspath | rev | awk -F '.' '{print $1}' | rev)
+  arrpath=(mp4 gif webm mkv)
+  if [[ ${arrpath[@]} =~ $strpath ]]; then
+    counter=1
+    dir
+  elif [[ $anspath == "Exit" ]] || [[ $anspath == "exit" ]] || [[ $anspath == "EXIT" ]]; then
+    fn_bye
+  elif [[ $anspath == "Back" ]] || [[ $anspath == "back" ]] || [[ $anspath == "BACK" ]]; then
+    clear
+    mainmenu
+  elif [[ ! -n $anspath ]]; then
+    pathsubmenu
+  else
+    fn_fail
+  fi
 }
 
 mainmenu() {
-    clear
-    echo -ne "
-MAIN MENU
-1) URL
-2) PATH
-0) Exit
-Choose an option: "
-    read -r ansmenu
-    case $ansmenu in
-    1|URL)
-        urlsubmenu
-        ;;
-    2|PATH)
-        pathsubmenu
-        ;;
-    0|Exit)
-        fn_bye
-        ;;
+  clear
+  echo -ne "MAINMENU\n1)URL\n2)PATH\n0)Exit\n\nChoose an option: "
+  read -r ansmenu
+  case $ansmenu in
+    1|Url|url|URL)
+      urlsubmenu;;
+    2|Path|path|PATH)
+      pathsubmenu;;
+    0|Exit|exit|EXIT)
+      fn_bye;;
     *)
-        fn_fail
-        ;;
-    esac
+      fn_fail;;
+  esac
 }
 
 mainmenu
